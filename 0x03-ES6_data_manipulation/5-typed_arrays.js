6299e553aa8222eef432a2276833442c330435e1
Xolelwa34 <ciliaswekile@gmail.com>
2024-10-08 09:20:51 -0700

    Set data structure

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const main = new Int8Main(buffer, 0, length);
+  const view = new DataView(buffer, 0, length);
 
   if (position >= length) {
     throw Error('Position outside range');
   }
-  main.setInt8(position, value);
-  return main;
+  view.setInt8(position, value);
+  return view;
 }
---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,3 @@
+export default function setFromArray(ary) {
+  return new Set(ary);
+}
